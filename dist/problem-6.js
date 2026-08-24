121db5cbe3cfab1a67ada854a5fcb38a0f6a072d
890asif <192508892+890asif@users.noreply.github.com>
2026-08-24 23:56:31 +0600

    container create

---
+++ dist/problem-6.js
@@ -0,0 +1,11 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const numberContainer = {
+    item: 100
+};
+const stringContainer = {
+    item: "Books"
+};
+console.log(stringContainer);
+console.log(numberContainer);
+//# sourceMappingURL=problem-6.js.map
