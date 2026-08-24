164e979320530ee636d7a68abefbf86e263782eb
890asif <192508892+890asif@users.noreply.github.com>
2026-08-24 23:47:43 +0600

    array last element return

---
+++ dist/problem-5.js
@@ -0,0 +1,9 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const getLastElement = (array) => {
+    return array[array.length - 1];
+};
+const array = [10, 20, 30];
+// const array = ["a", "b", "c"]
+console.log(getLastElement(array));
+//# sourceMappingURL=problem-5.js.map
