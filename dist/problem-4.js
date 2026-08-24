d3ba47afd81f804bd5985d750aac175c804df686
890asif <192508892+890asif@users.noreply.github.com>
2026-08-24 23:28:09 +0600

    payment method type check

---
+++ dist/problem-4.js
@@ -0,0 +1,8 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const tyoeCheck = (method) => {
+    return { id: 1, method: method };
+};
+const method = "cash";
+console.log(tyoeCheck(method));
+//# sourceMappingURL=problem-4.js.map
